4c6953365e26a7910e8e2255442225dcc2fcaf1c
Cameron Kerestus <125141520+Teamk09@users.noreply.github.com>
2023-04-29 00:15:53 -0400

    Battle System Logic
    Finished logic for the battle system
    
    Put battle theme into game
    
    Still need to implement bag and interactables

--- Assets/Battle Screen/BattleSystem.cs
+++ Assets/Battle Screen/BattleSystem.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
-public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST}
 
 public class BattleSystem : MonoBehaviour
 {
@@ -29,10 +30,10 @@ public class BattleSystem : MonoBehaviour
     void Start()
     {
         state = BattleState.START;
-        SetupBattle();
+        StartCoroutine(SetupBattle());
     }
 
-    void SetupBattle()
+    IEnumerator SetupBattle()
     {
         GameObject playerPokemon = Instantiate(PlayerPrefab, PlayerBattleStation);
         playerUnit = playerPokemon.GetComponent<Unit>();
@@ -44,6 +45,155 @@ public class BattleSystem : MonoBehaviour
 
         userHUD.SetHUD(playerUnit);
         enemyHUD.SetHUD(enemyUnit);
+
+        yield return new WaitForSeconds(3f);
+
+        state = BattleState.PLAYERTURN;
+        PlayerTurn();
+    }
+
+    void EndBattle()
+    {
+        if (state == BattleState.WON)
+            dialogueText.text = $"THE {enemyUnit.unitName} UNINSTALLS...".ToUpper();
+        else if (state == BattleState.LOST)
+        {
+            dialogueText.text = "YOU'VE BEEN INFECTED...";
+        }
+        else
+        {
+            dialogueText.text = "SUCCESS!";
+        }
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    IEnumerator EnemyTurn()
+    {
+        dialogueText.text = $"{enemyUnit.unitName} ATTACKS!".ToUpper();
+
+        yield return new WaitForSeconds(1f);
+
+        bool isDead = playerUnit.TakeDamage(enemyUnit.damage);
+
+        userHUD.HP(playerUnit.currentHP);
+
+        dialogueText.text = $"YOU TOOK {enemyUnit.damage} DAMAGE!";
+
+        yield return new WaitForSeconds(1f);
+
+        if(playerUnit.currentHP <=0)
+        {
+            state = BattleState.LOST;
+            EndBattle();
+        }
+        else
+        {
+            state = BattleState.PLAYERTURN;
+            PlayerTurn();
+        }
+
+    }
+
+
+    void PlayerTurn()
+    {
+        dialogueText.text = "CHOOSE AN ACTION.";
+    }
+
+    public void AttackButton()
+    {
+        if (state != BattleState.PLAYERTURN)
+            return;
+
+        StartCoroutine(PlayerAttack());
+
+    }
+    IEnumerator PlayerAttack()
+    {
+        bool isDead = enemyUnit.TakeDamage(playerUnit.damage);
+
+        enemyHUD.HP(enemyUnit.currentHP);
+        dialogueText.text = $"YOU DID {playerUnit.damage} DAMAGE";
+
+        state = BattleState.ENEMYTURN;
+
+        yield return new WaitForSeconds(2f);
+
+        if (isDead)
+        {
+            //End Battle
+            state = BattleState.WON;
+            EndBattle();
+        }
+
+        else
+        {
+            StartCoroutine(EnemyTurn());
+        }
+    }
+
+
+
+
+
+    public void HealButton()
+    {
+        if (state != BattleState.PLAYERTURN)
+            return;
+
+        StartCoroutine(PlayerHeal());
+
+    }
+
+    IEnumerator PlayerHeal()
+    {
+        playerUnit.Heal(5);
+
+        userHUD.HP(playerUnit.currentHP);
+        dialogueText.text = "YOU DRINK A COFFEE AND FEEL NEW AGAIN...";
+
+        yield return new WaitForSeconds(2f);
+
+        state = BattleState.ENEMYTURN;
+        StartCoroutine(EnemyTurn());
+    }
+
+
+
+
+
+    public void BagButton()
+    {
+        if (state != BattleState.PLAYERTURN)
+            return;
+
+        //StartCoroutine(PlayerBag());
+
+    }
+
+
+
+
+
+
+    public void FleeButton()
+    {
+        if (state != BattleState.PLAYERTURN)
+            return;
+
+        StartCoroutine(PlayerFlee());
+
+    }
+
+
+    IEnumerator PlayerFlee()
+    {
+        dialogueText.text = "YOU ATTEMPT TO FLEE";
+
+        yield return new WaitForSeconds(2f);
+
+        EndBattle();
     }
 
 }
